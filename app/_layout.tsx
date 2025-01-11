ebb9c173840c4f91786ee606cb8caf197c1c42b2
Eulier González <eulier1@gmail.com>
2025-01-11 13:18:33 -0300

    tab navigation

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,17 +1,39 @@
-import { Stack } from "expo-router";
+import { Tabs } from "expo-router";
+import Feather from "@expo/vector-icons/Feather";
+import AntDesign from "@expo/vector-icons/AntDesign";
+import FontAwesome5 from "@expo/vector-icons/FontAwesome5";
+import { theme } from "../theme";
 
 export default function Layout() {
   return (
-    <Stack>
-      <Stack.Screen
-        name="counter"
-        options={{ title: "Counter" }}
-      ></Stack.Screen>
-      <Stack.Screen
+    <Tabs screenOptions={{ tabBarActiveTintColor: theme.colorCerulean }}>
+      <Tabs.Screen
         name="index"
-        options={{ title: "Shopping List" }}
-      ></Stack.Screen>
-      <Stack.Screen name="idea" options={{ title: "Idea" }}></Stack.Screen>
-    </Stack>
+        options={{
+          title: "Shopping List",
+          tabBarIcon: ({ size, color }) => (
+            <Feather name="list" size={size} color={color} />
+          ),
+        }}
+      ></Tabs.Screen>
+      <Tabs.Screen
+        name="counter"
+        options={{
+          title: "Counter",
+          tabBarIcon: ({ size, color }) => (
+            <AntDesign name="clockcircleo" size={size} color={color} />
+          ),
+        }}
+      ></Tabs.Screen>
+      <Tabs.Screen
+        name="idea"
+        options={{
+          title: "Idea",
+          tabBarIcon: ({ size, color }) => (
+            <FontAwesome5 name="lightbulb" size={size} color={color} />
+          ),
+        }}
+      ></Tabs.Screen>
+    </Tabs>
   );
 }
--- app/counter.tsx
+++ app/counter.tsx
@@ -2,14 +2,8 @@ import { useRouter } from "expo-router";
 import { StyleSheet, View, Text, TouchableOpacity } from "react-native";
 
 export default function CounterScreen() {
-  const router = useRouter();
   return (
     <View style={styles.container}>
-      <TouchableOpacity onPress={() => router.navigate("/idea")}>
-        <Text style={{ textAlign: "center", marginBottom: 18, fontSize: 24 }}>
-          Go to /idea
-        </Text>
-      </TouchableOpacity>
       <Text style={styles.text}>Counter</Text>
     </View>
   );
