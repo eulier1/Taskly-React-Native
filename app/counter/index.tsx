d792498f4067fb00720103010d9aafebfc251c88
Eulier González <eulier1@gmail.com>
2025-01-23 12:02:16 -0300

    add schedule notifications

--- app/counter/index.tsx
+++ app/counter/index.tsx
@@ -1,11 +1,31 @@
-import { StyleSheet, View, Text, TouchableOpacity } from "react-native";
+import { StyleSheet, View, Text, TouchableOpacity, Alert } from "react-native";
 import { theme } from "../../theme";
 import { registerForPushNotificationsAsync } from "../../utils/registerForPushNotificationAsync";
+import * as Device from "expo-device";
+import * as Notification from "expo-notifications";
 
 export default function CounterScreen() {
   const handleRequestPermission = async () => {
     const result = await registerForPushNotificationsAsync();
     console.log(result);
+    if (result === "granted") {
+      await Notification.scheduleNotificationAsync({
+        content: {
+          title: "I'm a notification from your app",
+        },
+        trigger: {
+          type: Notification.SchedulableTriggerInputTypes.TIME_INTERVAL,
+          seconds: 10,
+        },
+      });
+    } else {
+      if (Device.isDevice) {
+        Alert.alert(
+          "Unable to schedule notification",
+          "Enable the notifications permission for Expo Go in setting",
+        );
+      }
+    }
   };
 
   return (
